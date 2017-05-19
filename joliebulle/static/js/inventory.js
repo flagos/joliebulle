b852030de3b531deba5214397f0cbd7de21132fd
flagos <flagospub@gmail.com>
2017-05-19 20:47:09 +0200

    Add dynamic fields

--- joliebulle/static/js/inventory.js
+++ joliebulle/static/js/inventory.js
@@ -1,8 +1,15 @@
 
 
-function addInput(divName, className){
-    var newdiv = document.createElement('div');
-    newdiv.innerHTML = '<input type="text" class="'+className+'-name-input form-control">';
-    document.getElementById(divName).appendChild(newdiv);
+function addInput(divName, unit){
+
+    document.getElementById(divName).innerHTML += '<br>';
+    classNames = ['inventory-name-input', 'inventory-qty-input'];
+    for (var i = 0, c = classNames.length; i < c; i++) {
+        // new_input = document.createElement('input');
+        // new_input.innerHTML = '<input type="text" class="'+classNames[i]+' form-control">';
+        var h = '<input type="text" class="'+classNames[i]+' form-control">';
+        document.getElementById(divName).innerHTML += h;
+    }
+    document.getElementById(divName).innerHTML += "<div class='inventory-unit'>"+unit+"</div>";
 
 }
