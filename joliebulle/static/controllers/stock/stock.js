b3432427292614f27eea9e1ebceaf98aff8fbf9d
flagos <flagospub@gmail.com>
2017-06-03 18:51:54 +0200

    Add some init to stock controller

--- joliebulle/static/controllers/stock/stock.js
+++ joliebulle/static/controllers/stock/stock.js
@@ -1,4 +1,25 @@
 toolsApp.controller('StockCtrl', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
 
     console.log('OKKKKKKK');
+
+    $scope.init = function () {
+        $scope.recipes = $scope.importRecipes();
+        $scope.recipes = _.chain($scope.recipes)
+            .sortBy(function (o) {return o.name.toLowerCase(); })
+            .sortBy(function (o) {return o.brewer.toLowerCase(); })
+            .value();
+        $scope.importIngredients();
+    };
+
+    $scope.importRecipes = function () {
+        return JSON.parse(main.dataRecipes().replace(/\bNaN\b/g, "null"));
+//        On remplace les Nan éventuellement produits en amont non compatibles json
+    };
+
+    $scope.importIngredients = function () {
+        $scope.ingredients = JSON.parse(main.dataIngredients());
+        $scope.ingredients = translate.translate_fr($scope.ingredients);
+    };
+
+
 }]);
