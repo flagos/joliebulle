8bc2b443c163110ee2ff25213df1459ceb4ff85f
flagos <flagospub@gmail.com>
2017-08-24 09:11:46 +0200

    Save inventory into a beerxml file.

--- joliebulle/static/beercalc/jbrecipe.js
+++ joliebulle/static/beercalc/jbrecipe.js
@@ -3,7 +3,7 @@
 var jbrecipe = (function () {
     "use strict";
     var recipe;
-    
+
     return {
         newRecipe : function () {
             recipe = {};
@@ -21,12 +21,12 @@ var jbrecipe = (function () {
             recipe.yeasts = [];
             recipe.mashProfile = {"name": "Infusion simple, corps moyen", "sparge": "78.0", "ph": "5.4", "tunTemp": "20", "steps": [{"name": "Emp\u00e2tage", "type_view": "Infusion", "type": "Infusion", "temp": "67.0", "time": "60"}, {"name": "Mash Out", "type_view": "Temp\u00e9rature", "type": "Temperature", "temp": "75.0", "time": "10"}]};
             recipe.notes = "";
-            
+
             return recipe;
         }
-    
-    
-    
+
+
+
     };
-    
+
 }());
--- joliebulle/static/controllers/stock/stock.js
+++ joliebulle/static/controllers/stock/stock.js
@@ -1,6 +1,5 @@
 toolsApp.controller('StockCtrl', ['$scope', '$http', '$filter', '$compile', function ($scope, $http, $filter, $compile) {
 
-    console.log('OKKKKKKK');
 
     $scope.init = function () {
         $scope.recipes = $scope.importRecipes();
@@ -22,7 +21,36 @@ toolsApp.controller('StockCtrl', ['$scope', '$http', '$filter', '$compile', func
     };
 
     $scope.saveInventory = function () {
-        console.log('IAMHEREEEEEEEE');
+        console.log('Saving inventory');
+        stock_recipe =  jbrecipe.newRecipe();
+        stock_recipe.path = "/tmp/stock.xml";
+
+        var malts = document.querySelectorAll('#inventory-malt input');
+        for (var i = 0; i <malts.length; i+=2){
+            var malt = {};
+            malt["name"] = malts[i].value;
+            malt["amount"] = malts[i+1].value * 1000; // display unit in kg
+            stock_recipe.fermentables.push(malt);
+        }
+
+        var hops = document.querySelectorAll('#inventory-hop input');
+        for (var i = 0; i <hops.length; i+=2){
+            var hop = {};
+            hop["name"] = hops[i].value;
+            hop["amount"] = hops[i+1].value;
+            stock_recipe.hops.push(hop);
+        }
+
+
+        var yeasts = document.querySelectorAll('#inventory-yeast input');
+        for (var i = 0; i <yeasts.length; i+=1){
+            var yeast = {};
+            yeast["name"] = yeasts[i].value;
+            stock_recipe.yeasts.push(yeast);
+        }
+
+        main.saveRecipe(jb2xml.exportString(stock_recipe), "/tmp/stock.xml");
+        // console.log(recipe);
     };
 
 
@@ -30,10 +58,13 @@ toolsApp.controller('StockCtrl', ['$scope', '$http', '$filter', '$compile', func
 
         angular.element(document.getElementById(divName)).append('<br>');
         classNames = ['inventory-name-input', 'inventory-qty-input'];
+        if(divName=="inventory-yeast"){
+            classNames = ['inventory-name-input'];
+        }
         for (var i = 0, c = classNames.length; i < c; i++) {
             // new_input = document.createElement('input');
             // new_input.innerHTML = '<input type="text" class="'+classNames[i]+' form-control">';
-            var h = '<input type="text" ng-click="saveInventory()" class="'+classNames[i]+' form-control">';
+            var h = '<input type="text" class="'+classNames[i]+' form-control">';
             var temp = $compile(h)($scope);
             angular.element(document.getElementById(divName)).append(temp);
         }
